a9a16f82b01a8ff308019ac8ceb2f1fbd71af01b
Shraddha Jain <shraddha@evenfinancial.com>
2022-07-20 11:45:35 -0400

    such a dumb mistake, figured it out

--- app/js/script.js
+++ app/js/script.js
@@ -11,26 +11,21 @@ Detect keypresses
 
 document.addEventListener("DOMContentLoaded", () => {  
 
-    console.log("are we here");
 const lettersPattern = /[a-z]/;
-let currentGuessCount = 0;
-let currentGuess = document.querySelector('.guess' + currentGuessCount);
-console.log(currentGuess)
-let matches = document.querySelectorAll('div.guess');
-console.log(matches);
+let currentGuessCount = 1;
+let currentGuess = document.querySelector("#guess" + currentGuessCount);
 
 //detect keypress (letter/backspace/other)
 document.addEventListener('keydown', (e) => {
-    console.log('keypress:' + e.key);
 
     //if letter, do this
     console.log(currentGuess);
     const key = e.key;
-    if (currentGuessCount < 6){
+    if (currentGuessCount < 7){
         if (key.length == 1 && 
-            lettersPattern.test(key)
+            lettersPattern.test(key) &&
+            currentGuess.dataset.letters.length < 6
             ) {
-                console.log("updating guess with:" + key);
                updateLetters(key);
                
             } else if (key == 'Backspace') {
@@ -45,7 +40,7 @@ const deleteFromLetters = () => {
     let oldLetters = currentGuess.dataset.letters;
     let newLetters = oldLetters.slice(0, -1);
     currentGuess.dataset.letters = newLetters;
-    console.log(currentGuess.dataset.letters);
+    console.log("current guess: "+currentGuess.dataset.letters);
 }
 
 const updateLetters = (letter) => {
@@ -53,7 +48,7 @@ const updateLetters = (letter) => {
     let newLetters = oldLetters + letter;
     let currentTile = newLetters.length;
     currentGuess.dataset.letters = newLetters;
-    console.log(currentGuess.dataset.letters);
+    console.log("current guess: "+currentGuess.dataset.letters);
     // updateTiles(currentTile, letter);
   };
 
